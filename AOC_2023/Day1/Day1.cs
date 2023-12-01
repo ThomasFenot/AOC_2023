5b94f83bb98abc971da1c520b62c5455c9e05562
tfenot <fenot@abyssecorp.com>
2023-12-01 15:35:22 +0100

    AOC : Day - 1 Chapter - 1
    Chapter 1 + Init

---
+++ AOC_2023/Day1/Day1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace AOC_2023.Day1
+{
+    public class Day1
+    {
+        public void ChapterOne() {
+            string[]  puzzleInputs = File.ReadAllLines(@"Day1\\input.txt"); 
+            int result = 0;
+
+            foreach (string currInput in puzzleInputs)
+            {
+                var resultString = string.Join(string.Empty, Regex.Matches(currInput, @"\d+").OfType<Match>().Select(m => m.Value));
+                List<string> strings = resultString.Select(c => c.ToString()).ToList();
+                string concat = strings.First() + strings.Last();
+                result += int.Parse(concat);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ AOC_2023/Program.cs
@@ -0,0 +1,18 @@
+﻿using AOC_2023.Day1;
+using System.Text.RegularExpressions;
+
+namespace AOC_2023
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("---------- AOC 2023 ----------");
+            Console.WriteLine("---------- Day 1 - Chapter 1 ----------");
+            Day1.Day1 day1 = new Day1.Day1();
+            day1.ChapterOne();
+            Console.WriteLine("---------- Day 1 - Chapter 2 ----------");
+
+        }
+    }
+}
